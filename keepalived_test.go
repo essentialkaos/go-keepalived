76988e0dc41f84b9df06a3df6fbf42f699a7b872
Anton Novojilov <andy@essentialkaos.com>
2019-08-23 13:53:29 +0300

    The very first version

---
+++ keepalived_test.go
@@ -0,0 +1,62 @@
+package keepalived
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                     Copyright (c) 2009-2019 ESSENTIAL KAOS                         //
+//        Essential Kaos Open Source License <https://essentialkaos.com/ekol>         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"testing"
+
+	. "pkg.re/check.v1"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func Test(t *testing.T) { TestingT(t) }
+
+type KASuite struct{}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+var _ = Suite(&KASuite{})
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func (s *KASuite) TestParsing(c *C) {
+	ip, err := GetVirtualIP("testdata/1.conf")
+
+	c.Assert(ip, Equals, "192.168.1.2")
+	c.Assert(err, IsNil)
+}
+
+func (s *KASuite) TestParsingErrors(c *C) {
+	ip, err := GetVirtualIP("testdata/unknown.conf")
+
+	c.Assert(ip, Equals, "")
+	c.Assert(err, NotNil)
+
+	ip, err = GetVirtualIP("testdata/2.conf")
+
+	c.Assert(ip, Equals, "")
+	c.Assert(err, NotNil)
+
+	ip, err = GetVirtualIP("testdata/3.conf")
+
+	c.Assert(ip, Equals, "")
+	c.Assert(err, NotNil)
+}
+
+func (s *KASuite) TestRoleChecker(c *C) {
+	isMaster, err := IsMaster("192.168.0.100")
+
+	c.Assert(isMaster, Equals, false)
+	c.Assert(err, IsNil)
+}
+
+func (s *KASuite) TestAux(c *C) {
+	c.Assert(extractIP("  abcd  "), Equals, "")
+	c.Assert(extractIP("  abcd abcd  "), Equals, "")
+}
